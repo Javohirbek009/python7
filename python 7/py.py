b893887c0b754d3751637e65d90784d5eeb4f244
Javohirbek009 <104622975+Javohirbek009@users.noreply.github.com>
2022-04-29 12:14:16 +0300

    Add files via upload

---
+++ python 7/py.py
@@ -0,0 +1,18 @@
+# pip va virtual environment
+
+# svetofor
+
+import time
+
+def color():
+    print("Stop...")
+    time.sleep(1)
+    print("🔴")
+    time.sleep(1)
+    print("🟡")
+    time.sleep(1)
+    print("🟢")
+    time.sleep(1)
+    print("Start")
+
+color()
